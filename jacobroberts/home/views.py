e1b014e7482139d520bbf299bda5008740cfd419
Jacob Roberts <j2late@gmail.com>
2024-12-23 17:57:50 -0600

    contact form works

--- jacobroberts/home/views.py
+++ jacobroberts/home/views.py
@@ -1,22 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.core.mail import send_mail
-
+import os
 # Create your views here.
 def index(request):
     if request.method == "POST":
+        s_email = os.environ['EMAIL_USERNAME']
         email = request.POST.get("email")
         first_name = request.POST.get("fname")
         last_name = request.POST.get("lname")
         subject = f"[{email}]: " + request.POST.get("subject")
         message = f"{first_name} {last_name} \n\n" + request.POST.get("message")
         print(message)
-        # send_mail(
-        #     subject,
-        #     message,
-        #     "jacob@jacobroberts.us",
-        #     ["jacob@jacobroberts.us"],
-        #     fail_silently=False,
-        # )
+        send_mail(
+            subject,
+            message,
+            s_email,
+            [s_email],
+            fail_silently=False,
+        )
 
     return render(request, "home.html")
