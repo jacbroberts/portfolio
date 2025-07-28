ed84922a7f04abe04c8d3e423ba91e0b06857c28
Jacob Roberts <j2late@gmail.com>
2025-07-27 23:13:21 -0500

    simple anti-spam for email feature

--- jacobroberts/home/views.py
+++ jacobroberts/home/views.py
@@ -2,6 +2,22 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.core.mail import send_mail
 import os
+
+def is_spam(message):
+    spam = False
+    malware = False
+    spam_words = ["price", "SEO", "free", "deadline","urgent"]
+    possible_malware_words = ["script.google.com","https","http"]
+    if any(word in message.lower() for word in spam_words):
+        spam = True
+        malware = True
+    if malware:
+        return "Possible Malware "
+    if spam:
+        return "SPAM "+ message
+    return message
+    
+
 # Create your views here.
 def index(request):
     if request.method == "POST":
@@ -10,7 +26,9 @@ def index(request):
         first_name = request.POST.get("fname")
         last_name = request.POST.get("lname")
         subject = f"[{email}]: " + request.POST.get("subject")
+        subject = is_spam(subject)
         message = f"{first_name} {last_name} \n\n" + request.POST.get("message")
+        message = is_spam(message)
         print(message)
         send_mail(
             subject,
